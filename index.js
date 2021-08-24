b4868fa3843470d9043a107a1ea68d88fc5ed3cf
Kemanth <31121518+Kemanth@users.noreply.github.com>
2021-08-25 01:13:24 +0530

    added auth

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
+const authRoutes = require('./routes/authRoutes');
+const mongoose = require('mongoose');
+const keys = require('./config/keys')
+
+require('./models/User');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
+
+app.use(authRoutes);
 
 app.get('/', (req, res) => {
     res.send({hi:'there'});
---
+++ routes/authRoutes.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const passport = require('passport');
+require('../models/User');
+require('../services/passport')(passport)
+
+const router = express.Router();
+
+router.get('/auth/google', 
+    passport.authenticate('google', {
+        scope : ['profile', 'email']
+    })
+);
+
+router.get('/auth/google/callback', 
+    passport.authenticate('google')
+);
+
+router.get('/api/logout', (req, res) => {
+    req.logOut();
+    res.send(req.user);
+})
+
+router.get('/api/current_user', (req, res) => {
+    res.send(req.user);
+})
+
+
+module.exports = router;
